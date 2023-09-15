ceff00d4744025c5450e57eb0d6701a1219a4a3e
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-09-15 15:51:36 +0500

    Task2

---
+++ Tyuiu.MedvedevA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MedvedevA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MedvedevA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+
+
+
+            Assert.AreEqual("Привет..., Игорь", res);  
+        }
+    }
+}
